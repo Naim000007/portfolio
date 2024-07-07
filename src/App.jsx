cce798a3c6892401473ed25e8ef366cd9a16e04d
Naim000007 <mdnaim01303202218@gmail.com>
2024-07-08 02:19:48 +0600

    project set up done

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./app.scss"
+const App = () => {
+  return (
+    <>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>contact</section>
+    </>
+
+
+  )
+}
+
+export default App
